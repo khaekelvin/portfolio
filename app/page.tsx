ec8718a421c84f038ab8cde963c04465e7ca034a
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-18 17:15:34 +0000

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ function Hero() {
   return (
     <>
       <div className="max-w-7xl mx-auto">
-        <div className="text-center text-3xl mb-6">
+        <div className="text-center text-3xl mb-6 pt-20 font-normal">
           King is a Web Developer in Accra, Ghana
         </div>
 
-        <div>
+        <div className="mt-16">
           <section className="flex gap-4 justify-center">
             <div className="lg:flex">
               <a data-tooltip-id="contact-tooltip" data-tooltip-place="bottom">
@@ -86,7 +86,7 @@ function Hero() {
               </a>
             </div>
 
-            <div className="lg:flex">
+            <div className="lg:flex mb-16">
               <a data-tooltip-id="in-tooltip" data-tooltip-place="bottom">
                 <div className="example-container __react_component_tooltip __react_component_tooltip.show rounded-3xl w-20 h-20 bg-customAsh hover:border-4 hover:bg-gray-400 flex justify-center">
                   <FaLinkedinIn size={30} className="my-auto" />
@@ -103,7 +103,7 @@ function Hero() {
             </div>
           </section>
 
-          <div className="border border-white h-auto w-40 rounded-3xl flex justify-center mx-auto ">
+          <div className="border border-white h-auto w-40 rounded-3xl flex justify-center mx-auto mb-7">
             <p className="my-1">Web Developer</p>
           </div>
 
