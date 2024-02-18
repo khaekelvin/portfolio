14cd8152e21c7f5d1fe859c95ca697bcc8b58b34
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-18 15:44:32 +0000

    top 2

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FaPaperPlane, FaLink } from "react-icons/fa";
 import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip } from "react-tooltip";
+import { RxExternalLink } from "react-icons/rx";
 
 function Hero() {
   return (
@@ -13,21 +14,39 @@ function Hero() {
         </div>
 
         <div>
-          <div className="lg:flex ">
-            <a
-              data-tooltip-id="my-tooltip-rounded"
-              data-tooltip-content="Contact Me"
-            >
-              <div className="example-container __react_component_tooltip __react_component_tooltip.show rounded-3xl w-20 h-20 bg-customAsh hover:border-4 hover:bg-gray-400 flex justify-center ">
-                <FaPaperPlane size={30} className="my-auto " />
-                <Tooltip
-                  id="my-tooltip-rounded"
-                  className="example-rounded __react_component_tooltip __react_component_tooltip.show hidden lg:flex"
-                  arrowColor="transparent"
-                />
-              </div>
-            </a>
-          </div>
+          <section className="flex gap-4 justify-center">
+            <div className="lg:flex">
+              <a data-tooltip-id="my-tooltip-rounded">
+                <div className="example-container __react_component_tooltip __react_component_tooltip.show rounded-3xl w-20 h-20 bg-customAsh hover:border-4 hover:bg-gray-400 flex justify-center">
+                  <FaPaperPlane size={30} className="my-auto" />
+                  <Tooltip
+                    id="my-tooltip-rounded"
+                    className="example-rounded __react_component_tooltip __react_component_tooltip.show hidden lg:flex"
+                    arrowColor="transparent"
+                  >
+                    <p className="mr-1">Contact Me</p>
+                    <RxExternalLink size={20} />
+                  </Tooltip>
+                </div>
+              </a>
+            </div>
+
+            <div className="lg:flex">
+              <a data-tooltip-id="my-tooltip-rounded">
+                <div className="example-container __react_component_tooltip __react_component_tooltip.show rounded-3xl w-20 h-20 bg-customAsh hover:border-4 hover:bg-gray-400 flex justify-center">
+                  <FaPaperPlane size={30} className="my-auto" />
+                  <Tooltip
+                    id="my-tooltip-rounded"
+                    className="example-rounded __react_component_tooltip __react_component_tooltip.show hidden lg:flex"
+                    arrowColor="transparent"
+                  >
+                    <p className="mr-1">Contact Me</p>
+                    <RxExternalLink size={20} />
+                  </Tooltip>
+                </div>
+              </a>
+            </div>
+          </section>
         </div>
       </div>
     </>
@@ -35,23 +54,3 @@ function Hero() {
 }
 
 export default Hero;
-
-{
-  /* <FaLink />
-  <div className="white-container">
-            <a
-              data-tooltip-id="my-tooltip"
-              data-tooltip-content=""
-              className="w-20 left-0 flex rounded-3xl"
-            >
-              <Tooltip
-                id="my-tooltip"
-                place="bottom"
-                className="example-rounded"
-              />
-              <div className="w-20 h-20 bg-black">
-                <FaPaperPlane size={25} />
-              </div>
-            </a>
-          </div> */
-}
